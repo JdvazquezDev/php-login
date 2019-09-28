061f909120aefee46f9c6bca68fadd27bacb3dc6
Jose <jdvazquezdev@gmail.com>
2019-09-29 00:18:48 +0200

    Add file structure
    Create php and css files

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login</title>
+   <link rel="stylesheet" href="assets/css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+
+  <h1>Login</h1>
+  <form action="login.php" method="post">
+    <input type="text" name="email" placeholder="Enter your email">
+    <input type="password" name="password" placeholder="Enter your password">
+    <input type="submit" value="Send form">
+  </form>
+</body>
+</html>
