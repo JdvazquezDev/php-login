0666e804862f3a118c4442d209317f967d06ad47
Jose <jdvazquezdev@gmail.com>
2019-10-02 13:08:23 +0200

    Añade crear usuario en signup

--- signup.php
+++ signup.php
@@ -1,6 +1,22 @@
 
 <?php 
     require 'database.php';
+
+    $message = '';
+
+    if (!empty($_POST['email'])&& !empty($_POST['password'])) {
+       $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+       $stmt = $conn->prepare($sql);
+       $stmt->bindParam(':email', $_POST['email']);
+       $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+       $stmt->bindParam(':password', $password);
+
+       if ($stmt->execute()) {
+           $message = 'Successfully created new user';
+       }else {
+           $message = 'Error';
+       }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
