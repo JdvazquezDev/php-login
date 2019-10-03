b5ee48e45d8817e4ff2aa7ed77de8aeb501dfc08
José Domínguez Vázquez <jdvazquezdev@gmail.com>
2019-10-04 00:38:16 +0200

    Add functionality index.php and finish example
    All works

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
+<?php
+session_start();
+
+require 'database.php';
+
+if (isset($_SESSION['user_id'])) {
+    $sql = "SELECT id, email, password FROM users WHERE id = :id";
+    $records = $conn->prepare($sql);
+    $records->bindParam(':id', $_SESSION['user_id']);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+
+    $user = null;
+
+    if (count($results) > 0) {
+        $user = $results;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,12 +29,20 @@
     <link rel="stylesheet" href="assets/css/styles.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 </head>
-<body>
 
+<body>
     <?php require 'partials/header.php' ?>
-    
-    <h1>Please login or signUp</h1>
+
+    <?php if (!empty($user)) : ?>
+    <br> Welcome. <?= $user['email']; ?>
+    <br>You are Successfully Logged In
+    <a href="logout.php">Logout</a>
+    <?php else : ?>
+    <h1>Please Login or SignUp</h1>
+
     <a href="login.php">Login</a> or
     <a href="signup.php">SignUp</a>
+    <?php endif; ?>
 </body>
+
 </html>
